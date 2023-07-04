606fa5c3ada704bf71d2a8c46440d5c9e92d7e5c
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-07-04 20:57:04 +0700

    Add Model from Database to Web Page

--- src/main/java/com/project/HRPlus/controller/employeeController.java
+++ src/main/java/com/project/HRPlus/controller/employeeController.java
@@ -3,8 +3,11 @@ package com.project.HRPlus.controller;
 
 import com.project.HRPlus.entity.Employee;
 import com.project.HRPlus.service.employeeService;
+import jakarta.servlet.http.HttpSession;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +19,9 @@ public class employeeController {
     private employeeService employeeService;
     
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+        List<Employee> employeeList = employeeService.getAllEmployee();
+        model.addAttribute("employee", employeeList);
         return "index";
     }
     
--- src/main/java/com/project/HRPlus/service/employeeService.java
+++ src/main/java/com/project/HRPlus/service/employeeService.java
@@ -3,6 +3,7 @@ package com.project.HRPlus.service;
 
 import com.project.HRPlus.entity.Employee;
 import com.project.HRPlus.repository.employeeRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +12,13 @@ public class employeeService {
     
     @Autowired
     private employeeRepository employeeRepository;
+    
     public void addEmployee(Employee employee) {
         employeeRepository.save(employee);
     }
+    
+    public List<Employee> getAllEmployee() {
+        return employeeRepository.findAll();
+    }
+    
 }
