4e29232ff1903ac236a5c5e666ce2f4a85af2b19
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-07-04 20:34:21 +0700

    Connect to MySQL Database

--- src/main/java/com/project/HRPlus/controller/employeeController.java
+++ src/main/java/com/project/HRPlus/controller/employeeController.java
@@ -2,6 +2,8 @@
 package com.project.HRPlus.controller;
 
 import com.project.HRPlus.entity.Employee;
+import com.project.HRPlus.service.employeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class employeeController {
+    @Autowired
+    private employeeService employeeService;
     
     @GetMapping("/")
     public String home(){
@@ -25,6 +29,7 @@ public class employeeController {
     public String registerEmployee(@ModelAttribute Employee employee) {
         
         System.out.println(employee);
-        return "add_employee";
+        employeeService.addEmployee(employee);
+        return "redirect:/";
     }
 }
--- src/main/java/com/project/HRPlus/repository/employeeRepository.java
+++ src/main/java/com/project/HRPlus/repository/employeeRepository.java
@@ -3,8 +3,9 @@ package com.project.HRPlus.repository;
 
 import com.project.HRPlus.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface employeeRepository extends JpaRepository<Employee, Integer>{
     
 }
