454e9946e8ba8b072c02267dcc7104eded70fca7
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-07-04 20:06:06 +0700

    Created Entity

---
+++ src/main/java/com/project/HRPlus/entity/Employee.java
@@ -0,0 +1,85 @@
+
+package com.project.HRPlus.entity;
+
+
+public class Employee {
+    
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String email;
+    private String telephoneNumber;
+    private int salary;
+    private String department;
+
+    public Employee() {
+    }
+
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    
+    
+}
