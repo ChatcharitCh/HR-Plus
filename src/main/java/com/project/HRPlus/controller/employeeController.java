20f58e9b8dae0d38adfe8fd1df00dc9bd9fc987d
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-07-04 19:52:56 +0700

    Create add_employee.html

--- src/main/java/com/project/HRPlus/controller/employeeController.java
+++ src/main/java/com/project/HRPlus/controller/employeeController.java
@@ -1,13 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.project.HRPlus.controller;
 
-/**
- *
- * @author Chatcharit
- */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
 public class employeeController {
     
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+    
+    @GetMapping("/addemployee")
+    public String addEmployee() {
+        return "add_employee";
+    }
 }
